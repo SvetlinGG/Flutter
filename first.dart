d26e70aeddc7b301472b9d9eef175292ade82bd1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-03 01:12:02 +0100

    string operations

--- first.dart
+++ first.dart
@@ -1,4 +1,21 @@
 void main(){
-    print('Hello Dart');
+
+  // work with numbers
+    // int num1 = 1;
+    // double num2 = 2.2;
+    // print(num1);
+    // print(num2);
+    // print(num1 + num2);
+    // num1 += 2;
+    // print(num1);
+     int a = 2;
+     int b = 3;
+     print('The sum of numbers is: ${a + b}');
+
+  // work with strings
+    String firstName = 'Svetlin';
+    String lastName = 'Garabedyan';
+    print('My name is: ${firstName} $lastName');
+    print(firstName.length);
 }
 
