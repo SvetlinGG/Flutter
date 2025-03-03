de5ca9b493467d757676e8c2f3f6c7b9f768bb00
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-03 13:59:13 +0100

    string manipulation

---
+++ stringManipulation.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  personalInfo('Svetlin', "Garabedyan", 52, 'Dart');
+
+}
+void personalInfo(String firstName, String lastName, int age, String programming){
+
+  print('Hello I am ${firstName} ${lastName}, ${age} years old.\nAnd I learning ${programming}');
+
+
+}
