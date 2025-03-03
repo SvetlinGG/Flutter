28f4013a10001253f930d3b89c371af5b1cf061d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-03 11:57:28 +0100

    converter

---
+++ arithmeticOperations.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  int num1 = 10;
+  int num2 = 20;
+
+  print('Addition: $num1 + $num2 = ${num1 + num2}');
+  print('Subtraction : $num1 - $num2 = ${num1 - num2}');
+  print('Multiplication : $num1 * $num2 = ${num1 * num2}');
+  print('Division : $num1 / $num2 = ${num1 / num2}');
+}
---
+++ converter.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  converter(10);
+  converter(23);
+  converter(115);
+
+}
+
+void converter(km){
+
+  print('${km * 0.621371192}');
+}
