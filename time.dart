3a6c684fc941595c0027559728230b04643fba86
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-05 00:17:38 +0100

    time

--- time.dart
+++ time.dart
@@ -1,12 +1,27 @@
 void main(){
 
-  timer(60);
+  timer(325);
 
 }
 
 void timer(int minutes){
 
-  double hours = minutes / 60;
-  //int minutes = 0;
-  print('${hours}:${minutes}');
+  double hours = 0;
+  double newMinutes = minutes % 60;
+
+  if ( minutes > 59){
+    hours += 1;
+    //minutes -= 60;
+    print('${hours.toStringAsFixed(0)}:${newMinutes.toStringAsFixed(0)}');
+    if ( newMinutes < 10){
+    print('0${hours.toStringAsFixed(0)}:0${newMinutes.toStringAsFixed(0)}');
+  }else if (hours < 10 ){
+    print('0${hours.toStringAsFixed(0)}:${newMinutes.toStringAsFixed(0)}');
+  }
+  } 
+  
+  
+  
+  
+  
 }
