0370d5b8b915c5b77556331cd30d41cb4a8dfd4a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-04 23:58:15 +0200

    calculator -2

---
+++ calculator-2.dart
@@ -0,0 +1,10 @@
+import 'calculate.dart';
+
+void main() {
+  calculator(5, 25);
+}
+
+void calculator(int num1, int num2) {
+  print('The sum is ${num1 + num2}');
+  print('The multiply is ${num1 * num2}');
+}
