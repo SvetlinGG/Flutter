897257f569f26b7c62cc86959e5c5fb07b8e0f30
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-05 23:55:04 +0200

    positive or negative

---
+++ positive-negative.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num = 0;
+  if (num > 0) {
+    print("The number is positive");
+  } else if (num == 0) {
+    print('Zero');
+  } else {
+    print("The number is negative");
+  }
+}
